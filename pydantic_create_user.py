3a97f5c939ab2df31e51ce8808f7703d0054dc59
Ivan-81105 <ivan_fedorov_22122@mail.ru>
2026-01-27 11:35:38 +0300

    Знакомство с Pydantic

---
+++ pydantic_create_user.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
+
+
+class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя
+    """
+    id: HttpUrl
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserSchema
+
