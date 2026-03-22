15494c4b625f6aabc3560a91e2553dc258bf1d06
Ivan-81105 <ivan_fedorov_22122@mail.ru>
2026-03-22 16:15:43 +0300

    Настройки автотестов с Pydantic

--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 import pytest
 import allure
 from allure_commons.types import Severity
+
+from config import settings
 from tools.allure.Features import AllureFeature
 from tools.allure.epics import AllureEpic
 from tools.allure.stories import AllureStory
@@ -31,7 +33,7 @@ class TestFiles:
     @allure.severity(Severity.BLOCKER)
     @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_file(self, files_client: FilesClient):
-        request = CreateFileRequestSchema(upload_file='./testdata/files/image.png')
+        request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = CreateFileResponseSchema.model_validate_json(response.text)
 
@@ -60,7 +62,7 @@ class TestFiles:
     def test_create_file_with_empty_filename(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
             filename="",
-            upload_file="./testdata/files/image.png"
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
@@ -78,7 +80,7 @@ class TestFiles:
     def test_create_file_with_empty_directory(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
             directory="",
-            upload_file="./testdata/files/image.png"
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
